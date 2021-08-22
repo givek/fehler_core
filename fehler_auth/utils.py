dc3abf071a6282a0daa1b5d870f99a0b77563267
vivek <vivekgandharkar@outlook.com>
2021-08-22 21:12:11 +0530

    Created AuthToken Serializer with exception handler.

--- fehler_auth/utils.py
+++ fehler_auth/utils.py
@@ -1,5 +1,24 @@
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from six import text_type
+from rest_framework.views import exception_handler
+
+
+def custom_exception_handler(exc, context):
+    # Call REST framework's default exception handler first,
+    # to get the standard error response.
+    response = exception_handler(exc, context)
+
+    # Update the structure of the response data.
+    if response is not None:
+        customized_response = {}
+        customized_response["errors"] = {}
+        print(response.data.items())
+        for key, value in response.data.items():
+            customized_response["errors"][key] = value[0]
+
+        response.data = customized_response
+
+    return response
 
 
 class TokenGenerator(PasswordResetTokenGenerator):
