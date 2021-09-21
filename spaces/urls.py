ec3999d18348e87d5ef27313297a742f2e5e4a0e
Vivek Gandharkar <vivekgandharkar@outlook.com>
2021-09-21 20:17:28 +0530

    feat(views): add CreateSpace view to spaces.views

--- spaces/urls.py
+++ spaces/urls.py
@@ -5,4 +5,5 @@ from .views import CreateSpace, ListSpaces
 
 urlpatterns = [
     path("spaces/<int:user_id>", ListSpaces.as_view(), name="list_spaces"),
+    path("create_space", CreateSpace.as_view(), name="create_space"),
 ]
--- spaces/views.py
+++ spaces/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import AllowAny, IsAuthenticated
 
+from .serializers import SpaceSerializer
 from .models import SpaceMembership
 
 
@@ -19,3 +20,28 @@ class ListSpaces(APIView):
             for space_membership in space_memberships
         ]
         return Response(user_spaces, status=status.HTTP_200_OK)
+
+
+class CreateSpace(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        """
+        Create a new space with provided credentials.
+        """
+        space_serializer = SpaceSerializer(data=request.data)
+        if space_serializer.is_valid(raise_exception=True):
+            new_space = space_serializer.save()
+            if new_space:
+                space_memberships = SpaceMembership.objects.filter(
+                    user=request.data["owner"]
+                )
+                user_spaces = [
+                    {
+                        "id": space_membership.space_id,
+                        "name": space_membership.space.name,
+                    }
+                    for space_membership in space_memberships
+                ]
+                return Response(user_spaces, status=status.HTTP_200_OK)
+        return Response(space_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
