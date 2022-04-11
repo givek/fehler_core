aa5a742645a8a2b30441391b745c8810bd3947f2
Vivek Gandharkar <vivekgandharkar@outlook.com>
2022-04-11 09:28:36 +0530

    feat(projects): add `Risk` model

--- projects/admin.py
+++ projects/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Project, ProjectMembership
+from .models import Project, ProjectMembership, Risk
 
 
 admin.site.register(Project)
 admin.site.register(ProjectMembership)
+admin.site.register(Risk)
--- projects/models.py
+++ projects/models.py
@@ -32,3 +32,66 @@ class ProjectMembership(models.Model):
 
     def __str__(self):
         return self.user.email
+
+
+class Risk(models.Model):
+
+    # BACKGROUND = (
+    #     ("Finance", "Finance"),
+    #     ("Operations", "Operations"),
+    #     ("Security", "Security"),
+    # )
+
+    IMPACT = (("L", "Low"), ("M", "Medium"), ("H", "High"))
+
+    PROBABILITY = (("L", "Low"), ("M", "Medium"), ("H", "High"))
+
+    # PROBABILITY = (
+    #     ("0", "0%"),
+    #     ("10", "10%"),
+    #     ("20", "20%"),
+    #     ("30", "30%"),
+    #     ("40", "40%"),
+    #     ("50", "50%"),
+    #     ("60", "60%"),
+    #     ("70", "70%"),
+    #     ("80", "80%"),
+    #     ("90", "90%"),
+    #     (
+    #         "100",
+    #         "100%",
+    #     ),  # C = 100 in Roman. Because value can't be more than 2 numbers.
+    # )
+
+    name = models.CharField(max_length=120)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    description = models.TextField(blank=True, null=True)
+    mitigation_action = models.TextField(blank=True, null=True)
+
+    # background = models.CharField(max_length=50, choices=BACKGROUND, blank=False)
+    impact = models.CharField(max_length=2, choices=IMPACT, blank=False)
+    probability = models.CharField(max_length=2, choices=PROBABILITY, blank=False)
+
+    # probability_percentage = models.CharField(
+    #     max_length=3, choices=PROBABILITY, blank=False
+    # )
+    owner = models.ForeignKey(
+        "fehler_auth.User",
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name="risk_owner",
+    )
+    reporter = models.ForeignKey(
+        "fehler_auth.User",
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name="risk_reporter",
+    )
+    # status = models.CharField(max_length=120)
+    date_created = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
+
+    # @property
+    # def severity(self):
