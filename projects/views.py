a4b39d3a99bf88b8a8f97b66d220a29528f56f33
Vivek Gandharkar <vivekgandharkar@outlook.com>
2022-06-24 12:14:31 +0530

    refactor(projects): views and urls

--- projects/views.py
+++ projects/views.py
@@ -18,13 +18,44 @@ from boards.models import Board, Column, Task
 from boards.serializers import TaskSerializer
 
 
-class ProjectInfo(APIView):
+class Projects(APIView):
     permission_classes = [IsAuthenticated]
 
-    def get(self, request, space_name, project_name):
+    # def get(self, request, space_name):
+    #     """
+    #     Return a list of all projects a particular user is associated with.
+    #     """
+    #     project_memberships = ProjectMembership.objects.filter(user=request.user.id)
+    #     # only return projects from that particular space.
+    #     user_projects = [
+    #         {
+    #             "id": project_membership.project_id,
+    #             "name": project_membership.project.name,
+    #             "space": project_membership.project.space.name,
+    #         }
+    #         for project_membership in project_memberships
+    #         if project_membership.project.space.name == space_name
+    #     ]
+    #     return Response(user_projects, status=status.HTTP_200_OK)
+
+    def get(self, request, space_name, project_name=None):
         """
-        Return a list of all projects a particular user is associated with.
+        Return a project from a particular space with a user is associated with.
         """
+
+        if not project_name:
+            project_memberships = ProjectMembership.objects.filter(user=request.user.id)
+            # only return projects from that particular space.
+            user_projects = [
+                {
+                    "id": project_membership.project_id,
+                    "name": project_membership.project.name,
+                    "space": project_membership.project.space.name,
+                }
+                for project_membership in project_memberships
+                if project_membership.project.space.name == space_name
+            ]
+            return Response(user_projects, status=status.HTTP_200_OK)
 
         space = Space.objects.get(name=space_name)
 
@@ -35,30 +66,139 @@ class ProjectInfo(APIView):
             status=status.HTTP_200_OK,
         )
 
+    def post(self, request, space_name):
+        """
+        Create a new project with provided credentials.
+        """
+        # name, space, description, (request.user)
 
-class ListProjects(APIView):
+        # for now, use request.user
+        # TODO: lead
+
+        print("data", request.data)
+        print("user", request.user)
+
+        space = Space.objects.get(name=space_name)
+
+        project_serializer = ProjectSerializer(
+            data={"name": request.data["name"], "space": space.id}
+        )
+
+        if project_serializer.is_valid(raise_exception=True):
+            new_project = project_serializer.save()
+            if new_project:
+                new_project_membership = self.create_project_membership(
+                    request.user, new_project.id
+                )
+                project_memberships = ProjectMembership.objects.filter(
+                    user=request.user
+                )
+
+                project_board = Board.objects.create(
+                    name=f"{new_project.name}-board", project=new_project
+                )
+
+                Column.objects.create(
+                    title="Backlog", board=project_board, column_order=1
+                )
+                Column.objects.create(
+                    title="Doing", board=project_board, column_order=2
+                )
+                Column.objects.create(title="Done", board=project_board, column_order=3)
+
+                user_projects = [
+                    {
+                        "id": project_membership.project_id,
+                        "name": project_membership.project.name,
+                        "space": project_membership.project.space.name,
+                    }
+                    for project_membership in project_memberships
+                    if project_membership.project.space.name == space_name
+                ]
+                return Response(user_projects, status=status.HTTP_200_OK)
+        return Response(project_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def create_project_membership(self, user, project_id):
+        project = Project.objects.get(id=project_id)
+        user = ProjectMembership.objects.create(user=user, project=project)
+        user.save()
+
+    def put(self, request, space_name, project_name):
+        """
+        Update a project with provided credentials.
+        """
+        space = Space.objects.get(name=space_name)
+        space_projects = Project.objects.filter(space=space)
+        project = space_projects.get(name=project_name)
+        project_serializer = ProjectSerializer(project, data=request.data)
+        if project_serializer.is_valid(raise_exception=True):
+            project_serializer.save()
+            return Response(project_serializer.data, status=status.HTTP_200_OK)
+        return Response(project_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, space_name, project_name):
+        """
+        Delete a project with provided credentials.
+        """
+        space = Space.objects.get(name=space_name)
+        space_projects = Project.objects.filter(space=space)
+        project = space_projects.get(name=project_name)
+        # project = Project.objects.get(id=project_id)
+        project.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# class ProjectInfo(APIView):
+#     permission_classes = [IsAuthenticated]
+
+#     def get(self, request, space_name, project_name):
+#         """
+#         Return a list of all projects a particular user is associated with.
+#         """
+
+#         space = Space.objects.get(name=space_name)
+
+#         project = Project.objects.filter(space=space).get(name=project_name)
+
+#         return Response(
+#             {"id": project.id, "name": project.name, "space": space.name},
+#             status=status.HTTP_200_OK,
+#         )
+
+
+# class ListProjects(APIView):
+#     permission_classes = [IsAuthenticated]
+
+#     def get(self, request, space_name):
+#         """
+#         Return a list of all projects a particular user is associated with.
+#         """
+#         project_memberships = ProjectMembership.objects.filter(user=request.user.id)
+#         # only return projects from that particular space.
+#         user_projects = [
+#             {
+#                 "id": project_membership.project_id,
+#                 "name": project_membership.project.name,
+#                 "space": project_membership.project.space.name,
+#             }
+#             for project_membership in project_memberships
+#             if project_membership.project.space.name == space_name
+#         ]
+#         return Response(user_projects, status=status.HTTP_200_OK)
+
+
+class ProjectMember(APIView):
     permission_classes = [IsAuthenticated]
 
-    def get(self, request, space_name):
+    def get(self, request, space_name, project_name):
         """
-        Return a list of all projects a particular user is associated with.
+        Return a list of all tasks associated with a particular space of a particular user.
         """
-        project_memberships = ProjectMembership.objects.filter(user=request.user.id)
-        # only return projects from that particular space.
-        user_projects = [
-            {
-                "id": project_membership.project_id,
-                "name": project_membership.project.name,
-                "space": project_membership.project.space.name,
-            }
-            for project_membership in project_memberships
-            if project_membership.project.space.name == space_name
-        ]
-        return Response(user_projects, status=status.HTTP_200_OK)
-
-
-class AddProjectMember(APIView):
-    permission_classes = [IsAuthenticated]
+        project = get_object_or_404(Project, name=project_name)
+        project_members = project.get_members()
+        print("proejct meber", project_members)
+        serializer = UserSerializer(project_members, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request, space_name, project_name):
         """
@@ -75,6 +215,40 @@ class AddProjectMember(APIView):
             project_membership.save()
             return Response(status=status.HTTP_200_OK)
         return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+# class AddProjectMember(APIView):
+#     permission_classes = [IsAuthenticated]
+
+#     def post(self, request, space_name, project_name):
+#         """
+#         Add a user to a project.
+#         """
+#         space = Space.objects.get(name=space_name)
+#         user = User.objects.get(email=request.data["email"])
+#         if user in space.get_members():
+#             project = Project.objects.get(name=project_name, space__name=space_name)
+
+#             project_membership = ProjectMembership(
+#                 project=project, user=user, date_joined=timezone.now()
+#             )
+#             project_membership.save()
+#             return Response(status=status.HTTP_200_OK)
+#         return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+# class ProjectMembers(APIView):
+#     permission_classes = [IsAuthenticated]
+
+#     def get(self, request, space_name, project_name):
+#         """
+#         Return a list of all tasks associated with a particular space of a particular user.
+#         """
+#         project = get_object_or_404(Project, name=project_name)
+#         project_members = project.get_members()
+#         print("proejct meber", project_members)
+#         serializer = UserSerializer(project_members, many=True)
+#         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 # class CreateProject(APIView):
@@ -212,35 +386,20 @@ class ProjectTasks(APIView):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
-class ProjectMembers(APIView):
-    permission_classes = [IsAuthenticated]
-
-    def get(self, request, space_name, project_name):
-        """
-        Return a list of all tasks associated with a particular space of a particular user.
-        """
-        project = get_object_or_404(Project, name=project_name)
-        project_members = project.get_members()
-        print("proejct meber", project_members)
-        serializer = UserSerializer(project_members, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-
-class ListRisks(APIView):
+class Risks(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request, space_name, project_name):
         """
         Return a list of all tasks associated with a particular project.
         """
-        project = Project.objects.get(name=project_name)
+        space = Space.objects.get(name=space_name)
+        project = Project.objects.filter(space=space).get(name=project_name)
         risks = Risk.objects.filter(project=project.id)
         serializer = RiskSerializer(risks, many=True)
 
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-
-class CreateRisk(APIView):
     def post(self, request, space_name, project_name):
         """
         Create a new risk with provided credentials.
@@ -257,9 +416,7 @@ class CreateRisk(APIView):
                 return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(risk_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
-class UpdateRisk(APIView):
-    def put(self, request, risk_id):
+    def put(self, request, space_name, project_name, risk_id):
         """
         Update a risk with provided credentials.
         """
@@ -270,12 +427,65 @@ class UpdateRisk(APIView):
             return Response(risk_serializer.data, status=status.HTTP_200_OK)
         return Response(risk_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
-class DeleteRisk(APIView):
-    def delete(self, request, risk_id):
+    def delete(self, request, space_name, project_name, risk_id):
         """
         Delete a risk with provided credentials.
         """
         risk = Risk.objects.get(id=risk_id)
         risk.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# class ListRisks(APIView):
+#     permission_classes = [IsAuthenticated]
+
+#     def get(self, request, space_name, project_name):
+#         """
+#         Return a list of all tasks associated with a particular project.
+#         """
+#         project = Project.objects.get(name=project_name)
+#         risks = Risk.objects.filter(project=project.id)
+#         serializer = RiskSerializer(risks, many=True)
+
+#         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+# class CreateRisk(APIView):
+#     def post(self, request, space_name, project_name):
+#         """
+#         Create a new risk with provided credentials.
+#         """
+#         risk_serializer = RiskSerializer(data=request.data)
+#         if risk_serializer.is_valid(raise_exception=True):
+#             new_risk = risk_serializer.save()
+#             # risk_serializer.save()
+
+#             if new_risk:
+#                 project = Project.objects.get(name=project_name)
+#                 risks = Risk.objects.filter(project=project)
+#                 serializer = RiskSerializer(risks, many=True)
+#                 return Response(serializer.data, status=status.HTTP_200_OK)
+#         return Response(risk_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# class UpdateRisk(APIView):
+#     def put(self, request, risk_id):
+#         """
+#         Update a risk with provided credentials.
+#         """
+#         risk = Risk.objects.get(id=risk_id)
+#         risk_serializer = RiskSerializer(risk, data=request.data)
+#         if risk_serializer.is_valid(raise_exception=True):
+#             risk_serializer.save()
+#             return Response(risk_serializer.data, status=status.HTTP_200_OK)
+#         return Response(risk_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# class DeleteRisk(APIView):
+#     def delete(self, request, risk_id):
+#         """
+#         Delete a risk with provided credentials.
+#         """
+#         risk = Risk.objects.get(id=risk_id)
+#         risk.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
